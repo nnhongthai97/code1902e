47bdcf65822cbdd0d20f6446d9971f6b5a13824c
nnhongthai97 <nnhongthai97@gmail.com>
2019-06-26 00:42:18 +0700

    code php

---
+++ codephp/day1/var.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Biến trong php</h1>
+<h1>Biến là đối tượng , thứ dùng để lưu trữ thông tin và thay đổi giá trị trong
+    quá trình chạy</h1>
+<h1>Biến trong php bắt đầu bằng $</h1>
+<h1> dấu = là phép gán vế bên phải sang bên trái</h1>
+
+<h1>Một biến được khai báo bên ngoài một hàm có PHẠM VI TOÀN CẦU và chỉ có thể được truy cập bên ngoài một hàm</h1>
+
+<a href="https://www.w3schools.com/php/php_variables.asp"><h2>Link</h2></a>
+
+<h1>Một biến được khai báo trong một hàm có PHẠM VI ĐỊA PHƯƠNG và chỉ có thể được truy cập trong hàm đó</h1>
+<pre>
+    Quy tắc khai báo tên biến
+    thứ nhất tên biến không được có dấu cách
+    không được có dấu tiếng việt
+    thứ 2 tên biến chỉ được chứa các chữ cái và chữ số và _
+    thứ 3 tên biến phải bắt đầu bằng 1 chữ cái
+</pre>
+<?php
+$tensinhvien = "nguyen van an";
+echo $tensinhvien;
+?>
+</body>
+</html>
